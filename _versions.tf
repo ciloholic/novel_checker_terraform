722864a28f7210666055b919fb4957c7aa53f407
ciloholic <ciloholic0119@gmail.com>
2022-05-05 15:25:56 +0900

    terraform upgrade 1.1.8 => 1.1.9

--- _versions.tf
+++ _versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "1.1.8"
+  required_version = "1.1.9"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.17.0"
+      version = "4.20.0"
     }
   }
 }
